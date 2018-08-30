8fd1f82ad975c250fc03165d7d3e7f0eb43e44d9
JerryAa <jerrya15@yahoo.com>
2018-08-30 04:11:24 -0500

    Initial commit creating single linked list

---
+++ src/javaapplication1/Node.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author jerrya
+ * 
+ * 
+ * Linked list 
+ */
+public class Node {
+     Node next = null; 
+    
+    private int data; 
+    
+    public Node(int d){ 
+        this.data = d; 
+    }
+    
+}
