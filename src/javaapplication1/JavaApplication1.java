7d64725054070d53c658dcca4606d9325ebfa868
JerryAa <jerrya15@yahoo.com>
2018-08-30 04:04:39 -0500

    Format code

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -28,39 +28,38 @@ public class JavaApplication1 {
         }
 
         HashMap<Students, Integer> hsm = new HashMap<Students, Integer>();
-        int id1 = rnd.nextInt(100000); 
+        int id1 = rnd.nextInt(100000);
 
         Students st1 = new Students(id1);
         st1.setFirstName("John");
         st1.setLastName("Doe");
         hsm.put(st1, id1);
 
-        int id2 = rnd.nextInt(100000); 
+        int id2 = rnd.nextInt(100000);
         Students st2 = new Students(id2);
         st2.setFirstName("Mike");
         st2.setLastName("James");
-        
+
         hsm.put(st2, id2);
 
         for (Students tmp : hsm.keySet()) {
-            System.out.println("First Name: " + tmp.getFirstName() +"     " +  "Last Name: " + tmp.getLastName() + "     " + "ID: " + tmp.getId());
+            System.out.println("First Name: " + tmp.getFirstName() + "     " + "Last Name: " + tmp.getLastName() + "     " + "ID: " + tmp.getId());
 
         }
-        
-        ArrayList<Integer> arr = new ArrayList<Integer>(); 
-        for(int i = 0 ; i < 10; i ++) { 
-            arr.add(rnd.nextInt(100)); 
-            
+
+        ArrayList<Integer> arr = new ArrayList<Integer>();
+        for (int i = 0; i < 10; i++) {
+            arr.add(rnd.nextInt(100));
+
         }
-        for(int val: arr){ 
-            System.out.println(val); 
+        for (int val : arr) {
+            System.out.println(val);
         }
-             
-        String sentence = "This is so fun"; 
-        String [] arr2 = sentence.split(" "); 
-        
-        
-        for(String s : arr2 ){ 
+
+        String sentence = "This is so fun";
+        String[] arr2 = sentence.split(" ");
+
+        for (String s : arr2) {
             System.out.println(s);
         }
 
