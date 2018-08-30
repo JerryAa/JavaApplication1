ceabfa682369e0383cb613c0ef3cf313c99cffc3
JerryAa <jerrya15@yahoo.com>
2018-08-30 04:04:50 -0500

    Format code

--- src/javaapplication1/Students.java
+++ src/javaapplication1/Students.java
@@ -4,46 +4,48 @@
  * and open the template in the editor.
  */
 package javaapplication1;
-import java.io.*; 
-import java.util.*; 
-import java.math.*; 
-import java.time.*; 
+
+import java.io.*;
+import java.util.*;
+import java.math.*;
+import java.time.*;
 
 /**
  *
  * @author jerrya
  */
 public class Students {
+
     private String firstName;
-    private String lastName; 
-    private int id; 
-    public Students(int val) { 
-        this.id = val; 
-        
-    }
-    
-    public void setFirstName(String  value){ 
-    
-          this.firstName = value; 
-          
-    }
-    public String getFirstName(){ 
-              return firstName;   
+    private String lastName;
+    private int id;
+
+    public Students(int val) {
+        this.id = val;
 
     }
-    
-    public void  setLastName(String value) 
-    { 
-        this.lastName = value; 
+
+    public void setFirstName(String value) {
+
+        this.firstName = value;
+
     }
-    
-    public String getLastName() 
-    { 
-              return lastName; 
+
+    public String getFirstName() {
+        return firstName;
+
+    }
+
+    public void setLastName(String value) {
+        this.lastName = value;
+    }
+
+    public String getLastName() {
+        return lastName;
 
     }
 
     public int getId() {
-        return this.id; 
+        return this.id;
     }
 }
